57155b87e6721c18b491b81ff1c6d4589c2098ae
Jagannath Padhy <jagannath.padhy@ondc.org>
2026-05-04 19:21:40 +0530

    feat: add GPS validation utility, expand search flow coverage, and include docker-compose configuration.

---
+++ buyerNPTestAPI/src/Retail_eB2B_BuyerTest_1_2_5/common.js
@@ -0,0 +1,15 @@
+const { generateTests } = require("../centralizedUtilities/utilityFunctions");
+
+function quoteTests() {
+
+}
+
+function fulfillmentTests() {
+
+}
+
+module.exports = {
+    generateTests,
+    quoteTests,
+    fulfillmentTests
+}
